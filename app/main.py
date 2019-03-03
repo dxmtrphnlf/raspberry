995eb2dd954529480c344e02a3cf35fdccc4a460
lfrtv <lfrtvn@gmail.com>
2019-03-03 19:13:08 +0300

    fix setting config for initial laser and button

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from config import AppConfig
 from sensors import Laser, Button
 
 if __name__ == '__main__':
-    laser = Laser(7)
-    button = Button(12)
     with AppConfig() as app_config:
+        laser = Laser(7)
+        button = Button(12)
         while True:
             button.listen_onclick(laser.switch)
